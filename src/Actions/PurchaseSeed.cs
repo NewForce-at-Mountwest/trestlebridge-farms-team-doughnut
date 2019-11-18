280794eae3766f06277f9d9c497053dd8d9898dc
newforce38 <jessicaayerst@gmail.com>
2019-11-18 11:59:46 -0500

    finished purchase seeds

--- src/Actions/PurchaseSeed.cs
+++ src/Actions/PurchaseSeed.cs
@@ -13,27 +13,80 @@ namespace Trestlebridge.Actions {
             Console.WriteLine ("3. Sesame");
 
             Console.WriteLine ();
-            Console.WriteLine ("What are you buying today?");
+            Console.WriteLine ("Choose seed to purchase.");
 
             Console.Write ("> ");
             string choice = Console.ReadLine ();
 
-            switch (Int32.Parse(choice))
+            if (choice == "1")
             {
-                case 1:
-                    Console.WriteLine("purchasing Sunflower Seed, press enter to continue");
-                    Console.ReadLine();
-                    Farm.PurchaseResource<Sunflower>();
-                    break;
-                case 2:
-                    ChooseNaturalField.CollectInput(farm, new Wildflower());
-                    break;
-                case 3:
-                    ChoosePlowedField.CollectInput(farm, new Sesame());
-                    break;
-                default:
-                    break;
+
+                    Console.WriteLine("1. Plowed Field");
+                    Console.WriteLine("2. Natural Field");
+                    Console.WriteLine();
+                    Console.WriteLine("Choose a FIELD option");
+                    Console.Write("> ");
+                    string option = Console.ReadLine();
+                    if (option == "1")
+                {
+                    // List plowed fields and user inputs the number, which will be used as second parameter.
+                    int i;
+                    for(i = 0; i < farm.PlowedFields.Count; i++){
+                        Console.WriteLine($"{i} Plowed Field");}
+
+                        Console.WriteLine("Choose a Plowed Field");
+                        Console.Write("> ");
+                        string pickNumber = Console.ReadLine();
+                        int result = Int32.Parse(pickNumber);
+                        farm.PurchaseResource<Sunflower>(new Sunflower(), result);
+                    }
+                else if (option == "2")
+                {
+                    // List Natural Fields and give option of which one user wants to pick.
+
+                    int i;
+                    for(i = 1; i < farm.NaturalFields.Count; i++){
+                        Console.WriteLine($"{i} Natural Field");}
+
+                        Console.WriteLine("Choose a Natural Field");
+                        Console.Write("> ");
+                        string pickNumber = Console.ReadLine();
+                        int result = Int32.Parse(pickNumber);
+                        farm.PurchaseResource<Sunflower>(new Sunflower(), result);
+                    }
+                }
+
+               else if (choice == "2")
+
+                 {
+                    // List Natural Fields and give option of which one user wants to pick.
+
+                    int i;
+                    for(i = 1; i < farm.NaturalFields.Count; i++){
+                        Console.WriteLine($"{i} Natural Field");}
+
+                        Console.WriteLine("Choose a Natural Field");
+                        Console.Write("> ");
+                        string pickNumber = Console.ReadLine();
+                        int result = Int32.Parse(pickNumber);
+                        farm.PurchaseResource<Wildflower>(new Wildflower(), result);
+                    }
+
+              else if (choice == "3")
+
+                {
+
+                    int i;
+                    for(i = 1; i < farm.PlowedFields.Count; i++){
+                        Console.WriteLine($"{i} Plowed Field");}
+
+                        Console.WriteLine("Choose a Plowed Field");
+                        Console.Write("> ");
+                        string pickNumber = Console.ReadLine();
+                        int result = Int32.Parse(pickNumber);
+                        farm.PurchaseResource<Sesame>(new Sesame(), result);
+                    }
+
             }
         }
     }
-}
